72b8c15a23968233ac98d460b1fee912683d55f5
TaylorEvert <56089116+TaylorEvert@users.noreply.github.com>
2020-01-23 15:33:19 -0600

    VS Practice Website 2
    Contains:
    - Embedded youtube video
    - Some form of CSS change
    - JavaScript Animation

---
+++ PracticeWebsite2/PracticeWebsite2/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function myMove() {
+    var animation = document.getElementById("animation");
+    var position = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (position == 400) {
+            clearInterval(id);
+        } else {
+            position++;
+            animation.style.top = position + 'px';
+            animation.style.left = position + 'px';
+        }
+    }
+}
